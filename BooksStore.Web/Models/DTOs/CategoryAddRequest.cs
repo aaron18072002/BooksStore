7d15ad5edc6483a1f2a1c848770820b57b638164
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-28 14:41:29 +0700

    Create category dto for create purpose

---
+++ BooksStore.Web/Models/DTOs/CategoryAddRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BooksStore.Web.Models.DTOs
+{
+    public class CategoryAddRequest
+    {
+        [Required(ErrorMessage = "Category name is required")]
+        [MaxLength(40, ErrorMessage = "Category name must be less than 40 characters")]
+        public string? Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public Category ToCategory()
+        {
+            return new Category()
+            {
+                Name = this.Name,
+                DisplayOrder = this.DisplayOrder
+            };
+        }
+    }
+}
