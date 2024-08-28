67b206527a2d966ca6d709c0ecf67fafea387952
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-28 11:11:35 +0700

    Seed categoris data in db context

--- BooksStore.Web/Database/BooksStoreDbContext.cs
+++ BooksStore.Web/Database/BooksStoreDbContext.cs
@@ -10,5 +10,37 @@ namespace BooksStore.Web.Database
 
         }
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //seed data for category table
+            var categories = new List<Category>()
+            {
+                new Category()
+                {
+                    Id = 1,
+                    Name = "Action",
+                    DisplayOrder = 1
+                },
+                new Category()
+                {
+                    Id = 2,
+                    Name = "Scifi",
+                    DisplayOrder = 2
+                },
+                new Category()
+                {
+                    Id = 3,
+                    Name = "History",
+                    DisplayOrder = 3
+                },
+            };
+            foreach (var category in categories)
+            {
+                modelBuilder.Entity<Category>().HasData(category);
+            }
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
