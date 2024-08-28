b0fc88bb4dbd7ff9490eb4e71244ac4c42dc1bd9
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-28 10:10:16 +0700

    Create db context

---
+++ BooksStore.Web/Database/BooksStoreDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BooksStore.Web.Database
+{
+    public class BooksStoreDbContext : DbContext
+    {
+        public BooksStoreDbContext(DbContextOptions<BooksStoreDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
