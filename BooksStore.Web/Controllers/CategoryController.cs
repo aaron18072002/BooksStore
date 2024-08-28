54b7fc44ed048d70c312f94eb2b546c05a17f0b6
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-28 14:43:58 +0700

    Add reference to view and create logging as field for CategoryCOntroller

--- BooksStore.Web/Controllers/CategoryController.cs
+++ BooksStore.Web/Controllers/CategoryController.cs
@@ -8,18 +8,32 @@ namespace BooksStore.Web.Controllers
     public class CategoryController : Controller
     {
         private readonly BooksStoreDbContext _db;
-        public CategoryController(BooksStoreDbContext db)
+        private readonly ILogger<CategoryController> _logger;
+        public CategoryController(BooksStoreDbContext db, ILogger<CategoryController> logger)
         {
             this._db = db;
+            this._logger = logger;
         }
 
         [HttpGet]
         [Route("[action]")]
         public async Task<IActionResult> Index()
         {
+            this._logger.LogInformation("{ControllerName}.{MethodName} action method",
+                nameof(CategoryController), nameof(this.Index));
             var categories = await this._db.Categories.ToListAsync();
 
             return View(categories);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult Create()
+        {
+            this._logger.LogInformation("{ControllerName}.{MethodName} action method",
+                nameof(CategoryController), nameof(this.Create));
+
+            return View();
         }
     }
 }
