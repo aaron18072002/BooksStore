f8c6e79814599d7f6950db92667db7d148d543da
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-28 16:22:54 +0700

    Handle logic for create category

--- BooksStore.Web/Controllers/CategoryController.cs
+++ BooksStore.Web/Controllers/CategoryController.cs
@@ -20,18 +20,20 @@ namespace BooksStore.Web.Controllers
         [Route("[action]")]
         public async Task<IActionResult> Index()
         {
-            this._logger.LogInformation("{ControllerName}.{MethodName} action method",
+            this._logger.LogInformation("{ControllerName}.{MethodName} action get method",
                 nameof(CategoryController), nameof(this.Index));
             var categories = await this._db.Categories.ToListAsync();
 
-            return View(categories);
+            var categoriesResponse = categories.Select(c => c.ToCategoryResponse());
+
+            return View(categoriesResponse);
         }
 
         [HttpGet]
         [Route("[action]")]
         public IActionResult Create()
         {
-            this._logger.LogInformation("{ControllerName}.{MethodName} action method",
+            this._logger.LogInformation("{ControllerName}.{MethodName} action get method",
                 nameof(CategoryController), nameof(this.Create));
 
             return View();
@@ -42,18 +44,25 @@ namespace BooksStore.Web.Controllers
         public async Task<IActionResult> Create
             ([FromForm]CategoryAddRequest categoryAddRequest)
         {
-            if(categoryAddRequest == null)
+            this._logger.LogInformation("{ControllerName}.{MethodName} action post method",
+                nameof(CategoryController), nameof(this.Create));
+
+            if (categoryAddRequest == null)
             {
                 throw new ArgumentNullException(nameof(categoryAddRequest));
             }
 
-            var category = categoryAddRequest.ToCategory();
-            //category.Id = categoryAddRequest.DisplayOrder;
+            if(this.ModelState.IsValid)
+            {
+                var category = categoryAddRequest.ToCategory();
 
-            this._db.Categories.Add(category);
-            await this._db.SaveChangesAsync();
+                this._db.Categories.Add(category);
+                await this._db.SaveChangesAsync();
 
-            return RedirectToAction("Index", "Category");
+                return RedirectToAction("Index", "Category");
+            }
+
+            return View();
         }
     }
 }
