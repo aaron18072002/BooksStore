5af4fc34179567481f1d0adb340ea6624290ac51
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-28 09:13:25 +0700

    Create Category model

---
+++ BooksStore.Web/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BooksStore.Web.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Category name is required")]
+        [MaxLength(40, ErrorMessage = "Category name is too long")]
+        public string? Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
