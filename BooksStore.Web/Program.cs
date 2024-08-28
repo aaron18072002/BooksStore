e87640259f72b09620ee72120c66b6c8d4314ce3
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-28 10:11:01 +0700

    Register dbcontext into ioc as a service

--- BooksStore.Web/Program.cs
+++ BooksStore.Web/Program.cs
@@ -1,3 +1,6 @@
+using BooksStore.Web.Database;
+using Microsoft.EntityFrameworkCore;
+
 namespace BooksStore.Web
 {
     public class Program
@@ -8,6 +11,11 @@ namespace BooksStore.Web
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<BooksStoreDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             var app = builder.Build();
 
