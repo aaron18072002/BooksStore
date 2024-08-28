e1702221330a696a622e310502ebe535a18e8fc3
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-28 10:50:54 +0700

    Create index endpoint for category list view

---
+++ BooksStore.Web/Controllers/CategoryController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BooksStore.Web.Controllers
+{
+    [Route("[controller]")]
+    public class CategoryController : Controller
+    {
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
