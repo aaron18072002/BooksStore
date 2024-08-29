b39d119c80676e1c8d4de9ed5f40abf8515f0db4
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-29 14:54:11 +0700

    Initilize database and seed data for categories table

--- BooksStoreRazor.Temp/Database/BooksStoreRazorDbContext.cs
+++ BooksStoreRazor.Temp/Database/BooksStoreRazorDbContext.cs
@@ -12,6 +12,33 @@ namespace BooksStoreRazor.Temp.Database
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // seed catogories data
+            var categories = new List<Category>()
+            {
+                new Category()
+                {
+                    Id = 1,
+                    Name = "Action",
+                    DisplayOrder = 1
+                },
+                new Category()
+                {
+                    Id = 2,
+                    Name = "Scifi",
+                    DisplayOrder = 2
+                },
+                new Category()
+                {
+                    Id = 3,
+                    Name = "History",
+                    DisplayOrder = 3
+                },
+            };
+            foreach (var category in categories)
+            {
+                modelBuilder.Entity<Category>().HasData(category);
+            }
+
             base.OnModelCreating(modelBuilder);
         }
     }
