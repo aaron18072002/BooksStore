bd0113c842ab9e8f8398f8a890906dc8bf1ebfa7
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-09-02 10:02:12 +0700

    Create category repo

---
+++ BooksStore.DataAccess/Repositories/CategoryRepository.cs
@@ -0,0 +1,28 @@
+﻿using BooksStore.DataAccess.Database;
+using BooksStore.DataAccess.Repositories.IRepositories;
+using BooksStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksStore.DataAccess.Repositories
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(BooksStoreDbContext db) : base(db)
+        {
+
+        }
+        public Task Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
