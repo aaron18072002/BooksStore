1ec391be90e8eefdf44f1a2830fce2ddd808fe71
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-09-02 13:46:39 +0700

    Create product model

---
+++ BooksStore.Models/Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksStore.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string? Name { get; set; }
+
+        [Required]
+        [Display(Name = "International Standard Book Number")]
+        public string? ISBN { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string? Author { get; set; }
+
+        [Required]
+        [Display(Name = "List Price")]
+        [Range(1, 1000)]
+        public decimal ListPrice { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 1-50")]
+        [Range(1, 1000)]
+        public decimal Price { get; set; }
+
+
+        [Required]
+        [Display(Name = "Price for 50+")]
+        [Range(1, 1000)]
+        public decimal Price50 { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 100+")]
+        [Range(1, 1000)]
+        public decimal Price100 { get; set; }
+    }
+}
