ab815a7c7ae55ffe66de4ce9183892358803a9df
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-09-02 09:23:40 +0700

    Create IRepository base interface for implement repository pattern

---
+++ BooksStore.DataAccess/Repositories/IRepository/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksStore.DataAccess.Repositories.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+
+        T? GetDetails(Expression<Func<T, bool>> filter);
+
+        T Add(T entity);
+
+        bool Remove(T entity);
+
+        bool RemoveRange(IEnumerable<T> entities);
+    }
+}
