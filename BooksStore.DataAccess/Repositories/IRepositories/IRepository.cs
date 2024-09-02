931c4bb659db4477c186b6ff436466dabb0229f3
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-09-02 09:52:41 +0700

    Change namespace name and create repo interface for category

--- BooksStore.DataAccess/Repositories/IRepositories/IRepository.cs
+++ BooksStore.DataAccess/Repositories/IRepositories/IRepository.cs
@@ -5,7 +5,7 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BooksStore.DataAccess.Repositories.IRepository
+namespace BooksStore.DataAccess.Repositories.IRepositories
 {
     public interface IRepository<T> where T : class
     {
