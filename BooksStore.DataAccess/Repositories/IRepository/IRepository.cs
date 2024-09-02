8a734429151420e9f3b32be496049eb14c2da6aa
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-09-02 09:42:07 +0700

    Convert to asynchonous methods

--- BooksStore.DataAccess/Repositories/IRepository/IRepository.cs
+++ BooksStore.DataAccess/Repositories/IRepository/IRepository.cs
@@ -9,14 +9,14 @@ namespace BooksStore.DataAccess.Repositories.IRepository
 {
     public interface IRepository<T> where T : class
     {
-        IEnumerable<T> GetAll();
+        Task<IEnumerable<T>> GetAll();
 
-        T? GetDetails(Expression<Func<T, bool>> filter);
+        Task<T?> GetDetails(Expression<Func<T, bool>> filter);
 
-        T Add(T entity);
+        Task<T> Add(T entity);
 
-        bool Remove(T entity);
+        Task<bool> Remove(T entity);
 
-        bool RemoveRange(IEnumerable<T> entities);
+        Task<bool> RemoveRange(IEnumerable<T> entities);
     }
 }
