82fd60a093d857e5376209b790b90ce176f882c9
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-09-03 10:00:09 +0700

    Create foreign key by annotation

--- BooksStore.Models/Product.cs
+++ BooksStore.Models/Product.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -47,5 +48,10 @@ namespace BooksStore.Models
         [Display(Name = "Price for 100+")]
         [Range(1, 1000)]
         public decimal Price100 { get; set; }
+
+        public int CategoryId { get; set; }
+
+        [ForeignKey(nameof(this.CategoryId))]
+        public Category? Category { get; set; }
     }
 }
