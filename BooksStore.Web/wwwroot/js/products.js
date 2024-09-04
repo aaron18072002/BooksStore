272580021881661129a3e11635ac8b759eddf0f3
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-09-04 15:39:59 +0700

    Create get data table function by js

---
+++ BooksStore.Web/wwwroot/js/products.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/Admin/Product/GetAllProducts' },
+        "columns": [
+            { data: 'title', "width": "25%" },
+            { data: 'isbn', "width": "15%" },
+            { data: 'listPrice', "width": "10%" },
+            { data: 'author', "width": "20%" },
+            { data: 'categoryName', "width": "15%" }
+        ]
+    });
+}
