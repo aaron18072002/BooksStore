ed6f7ecb8b3794ed623b2c6bc51087570a82da0b
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-09-04 16:33:56 +0700

    Update jquery for edit and delete functionalities

--- BooksStore.Web/wwwroot/js/products.js
+++ BooksStore.Web/wwwroot/js/products.js
@@ -9,8 +9,18 @@ function loadDataTable() {
             { data: 'title', "width": "25%" },
             { data: 'isbn', "width": "15%" },
             { data: 'listPrice', "width": "10%" },
-            { data: 'author', "width": "20%" },
-            { data: 'categoryName', "width": "15%" }
+            { data: 'author', "width": "15%" },
+            { data: 'categoryName', "width": "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                     <a href="/Admin/Product/Edit?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>
+                     <a href="/Admin/Product/Delete?id=${data}" class="btn btn-danger mx-2"> <i class="bi bi-pencil-square"></i> Delete</a>
+                    </div>`
+                },
+                "width": "25%"
+            }
         ]
     });
 }
