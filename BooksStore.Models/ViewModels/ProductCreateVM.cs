14a80c3802661e5b447487d327090e9582b2948f
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-09-04 09:59:46 +0700

    Mofidy product create view model

--- BooksStore.Models/ViewModels/ProductCreateVM.cs
+++ BooksStore.Models/ViewModels/ProductCreateVM.cs
@@ -1,4 +1,6 @@
 ﻿using BooksStore.Models.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
@@ -10,7 +12,9 @@ namespace BooksStore.Models.ViewModels
 {
     public class ProductCreateVM
     {
-        public ProductAddRequest? ProductAddRequest { get; set; }
-        public IEnumerable<SelectListItem>? CategoriesList { get; set; }
+        public required ProductAddRequest ProductAddRequest { get; set; }
+
+        [ValidateNever]
+        public IFormFile? File { get; set; }
     }
 }
