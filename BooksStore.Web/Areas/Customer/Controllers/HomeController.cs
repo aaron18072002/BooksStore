e776bab73240100f4d1a5657e825eb6f10902c67
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-09-05 13:38:20 +0700

    Add logics to controller for get all products

--- BooksStore.Web/Areas/Customer/Controllers/HomeController.cs
+++ BooksStore.Web/Areas/Customer/Controllers/HomeController.cs
@@ -1,22 +1,46 @@
+using BooksStore.DataAccess.Repositories.IRepositories;
 using BooksStore.Models;
+using BooksStore.Models.DTOs;
+using BooksStore.Web.Areas.Admin.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace BooksStore.Web.Areas.Customer.Controllers
 {
     [Area("Customer")]
+    [Route("[area]/[controller]")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
-            _logger = logger;
+            this._logger = logger;
+            this._unitOfWork = unitOfWork;
         }
 
-        public IActionResult Index()
+        private ProductResponse ConvertProductToProductResponse(Product product)
         {
-            return View();
+            var productResponse = product.ToProductResponse();
+            productResponse.CategoryName = product?.Category?.Name;
+
+            return productResponse;
+        }
+
+        [HttpGet]
+        [Route("/")]
+        [Route("[action]")]
+        public async Task<IActionResult> Index()
+        {
+            this._logger.LogInformation("{ControllerName}.{MethodName} action get method",
+                nameof(HomeController), nameof(this.Index));
+
+            var products = await this._unitOfWork.Products.GetAll(includeProperties: "Category"); 
+
+            var productsResponse = products.Select(p => this.ConvertProductToProductResponse(p)).ToList();
+
+            return View(productsResponse);
         }
 
         public IActionResult Privacy()
