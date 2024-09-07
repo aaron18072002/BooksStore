37df361e31c499372b8c35ccde70059ddd655037
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-09-07 17:26:52 +0700

    Create company entity and its dtos class

---
+++ BooksStore.Models/DTOs/CompanyResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksStore.Models.DTOs
+{
+    public class CompanyResponse
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        [DisplayName("Street Address")]
+        public string? StreetAddress { get; set; }
+
+        [MaxLength(40)]
+        public string? City { get; set; }
+
+        [MaxLength(40)]
+        public string? State { get; set; }
+
+        [DisplayName("Postal Code")]
+        public string? PostalCode { get; set; }
+
+        [DisplayName("Phone Number")]
+        public string? PhoneNumber { get; set; }
+    }
+}
