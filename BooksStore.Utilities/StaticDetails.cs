e4472ddcc76ec5e1c5d8a46a58796ddc94b8250a
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-09-07 09:55:05 +0700

    Create some utility classes

---
+++ BooksStore.Utilities/StaticDetails.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksStore.Utilities
+{
+    public class StaticDetails
+    {
+        public const string Role_Customer = "Customer";
+        public const string Role_Company = "Company";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
+    }
+}
