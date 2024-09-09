bbef8a1d8a01ddfb98516b6a3b86237a6d70b0c7
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-09-09 09:26:32 +0700

    Add count field for shopping cart table

--- BooksStore.DataAccess/Migrations/20240909022216_Create_ShoppingCartsTable.Designer.cs
+++ BooksStore.DataAccess/Migrations/20240909022216_Create_ShoppingCartsTable.Designer.cs
@@ -4,6 +4,7 @@ using BooksStore.DataAccess.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BooksStore.DataAccess.Migrations
 {
     [DbContext(typeof(BooksStoreDbContext))]
-    partial class BooksStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240909022216_Create_ShoppingCartsTable")]
+    partial class Create_ShoppingCartsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -276,9 +279,6 @@ namespace BooksStore.DataAccess.Migrations
 
                     b.Property<string>("ApplicationUserId")
                         .HasColumnType("nvarchar(450)");
-
-                    b.Property<int>("Count")
-                        .HasColumnType("int");
 
                     b.Property<int>("ProductId")
                         .HasColumnType("int");
--- BooksStore.DataAccess/Migrations/20240909022527_AddCountField_ShoppingCartsTable.Designer.cs
+++ BooksStore.DataAccess/Migrations/20240909022527_AddCountField_ShoppingCartsTable.Designer.cs
@@ -4,6 +4,7 @@ using BooksStore.DataAccess.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BooksStore.DataAccess.Migrations
 {
     [DbContext(typeof(BooksStoreDbContext))]
-    partial class BooksStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240909022527_AddCountField_ShoppingCartsTable")]
+    partial class AddCountField_ShoppingCartsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
