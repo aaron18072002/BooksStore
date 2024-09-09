ec0a1b86a2706a8d707ec9d2cff965aa47c0eff6
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-09-09 09:26:17 +0700

    Add count field

--- BooksStore.Models/ShoppingCart.cs
+++ BooksStore.Models/ShoppingCart.cs
@@ -20,6 +20,10 @@ namespace BooksStore.Models
         [ValidateNever]
         public Product? Product { get; set; }
 
+        [Required(ErrorMessage = "Count is required")]
+        [Range(1, 1000, ErrorMessage = "Please enter a value between 1 and 1000")]
+        public int Count { get; set; }
+
         public string? ApplicationUserId { get; set; }
 
         [ForeignKey("ApplicationUserId")]
