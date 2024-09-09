4043054c585b95903e981b3e27a0747898e519de
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-09-09 10:29:48 +0700

    Create endpoint and view for cart

---
+++ BooksStore.Web/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BooksStore.Web.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Route("[area]/[controller]")]
+    public class CartController : Controller
+    {
+        private ILogger<CartController> _logger;
+        public CartController(ILogger<CartController> logger)
+        {
+            this._logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> Index()
+        {
+            this._logger.LogInformation("{ControllerName}.{MethodName} action get method",
+                nameof(CartController), nameof(this.Index));
+
+            return View();
+        }
+    }
+}
