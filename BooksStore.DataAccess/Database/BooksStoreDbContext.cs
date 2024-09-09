8da0d7d9711ccee008041aa6b84b527aa349e9c6
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-09-09 13:42:15 +0700

    Create OrderDetails and OrderHeaders tables in database

--- BooksStore.DataAccess/Database/BooksStoreDbContext.cs
+++ BooksStore.DataAccess/Database/BooksStoreDbContext.cs
@@ -14,6 +14,8 @@ namespace BooksStore.DataAccess.Database
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<Company> Companies { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
         public BooksStoreDbContext(DbContextOptions<BooksStoreDbContext> options) : base(options)
         {
 
--- BooksStore.DataAccess/Migrations/20240909064053_Create_OrderDetails_OrderHeadersTables.Designer.cs
+++ BooksStore.DataAccess/Migrations/20240909064053_Create_OrderDetails_OrderHeadersTables.Designer.cs
@@ -4,6 +4,7 @@ using BooksStore.DataAccess.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BooksStore.DataAccess.Migrations
 {
     [DbContext(typeof(BooksStoreDbContext))]
-    partial class BooksStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240909064053_Create_OrderDetails_OrderHeadersTables")]
+    partial class Create_OrderDetails_OrderHeadersTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
