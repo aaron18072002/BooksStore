3a8a16be6d94f26b2411ce831e7feafe73bd2e8b
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-09-12 11:21:03 +0700

    Add payment type field for order headers table

--- BooksStore.DataAccess/Migrations/20240912041358_AddPaymentTypeField_OrderHeadersTable.Designer.cs
+++ BooksStore.DataAccess/Migrations/20240912041358_AddPaymentTypeField_OrderHeadersTable.Designer.cs
@@ -4,6 +4,7 @@ using BooksStore.DataAccess.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BooksStore.DataAccess.Migrations
 {
     [DbContext(typeof(BooksStoreDbContext))]
-    partial class BooksStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240912041358_AddPaymentTypeField_OrderHeadersTable")]
+    partial class AddPaymentTypeField_OrderHeadersTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
