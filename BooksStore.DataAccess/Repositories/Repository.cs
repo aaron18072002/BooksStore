ca13ee3439fa80c8a6fa624fb749870fc475d380
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-09-12 10:01:32 +0700

    Add nullable to filter for disable warning

--- BooksStore.DataAccess/Repositories/Repository.cs
+++ BooksStore.DataAccess/Repositories/Repository.cs
@@ -11,87 +11,85 @@ using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace BooksStore.DataAccess.Repositories
 {
-    public class Repository<T> : IRepository<T> where T : class
-    {
-        protected readonly BooksStoreDbContext _db;
-        internal DbSet<T> DbSet;
-        public Repository(BooksStoreDbContext db)
-        {
-            this._db = db;
-            this.DbSet = this._db.Set<T>();
-            this._db.Products.Include(p => p.Category).Include(p => p.CategoryId);
-        }
+	public class Repository<T> : IRepository<T> where T : class
+	{
+		protected readonly BooksStoreDbContext _db;
+		internal DbSet<T> DbSet;
+		public Repository(BooksStoreDbContext db)
+		{
+			this._db = db;
+			this.DbSet = this._db.Set<T>();
+			this._db.Products.Include(p => p.Category).Include(p => p.CategoryId);
+		}
 
-        public async Task<T> Add(T entity)
-        {
-            this.DbSet.Add(entity);
-            await this._db.SaveChangesAsync();
+		public async Task<T> Add(T entity)
+		{
+			this.DbSet.Add(entity);
+			await this._db.SaveChangesAsync();
 
-            return entity;
-        }
+			return entity;
+		}
 
-        public async Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
-        {
-            IQueryable<T> result = this.DbSet;
-            if(filter != null)
-            {
-                result = result.Where(filter);
-            }
+		public async Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
+		{
+			IQueryable<T> result = this.DbSet;
+			if (filter != null)
+			{
+				result = result.Where(filter);
+			}
 
-            if (!string.IsNullOrEmpty(includeProperties))
-            {
-                foreach (var includeProp in includeProperties
-                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
-                {
-                    result = result.Include(includeProp);
-                }
-            }
+			if (!string.IsNullOrEmpty(includeProperties))
+			{
+				foreach (var includeProp in includeProperties
+					.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+				{
+					result = result.Include(includeProp);
+				}
+			}
 
-            return await result.ToListAsync();
-        }
+			return await result.ToListAsync();
+		}
 
-        public async Task<T?> GetDetails
-            (Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false)
-        {
-            IQueryable<T> result;
-            if(tracked)
-            {
-                result = this.DbSet;
-            }
-            else
-            {
-                result = this.DbSet.AsNoTracking();
-            }
+		public async Task<T?> GetDetails
+			(Expression<Func<T, bool>>? filter = null, string? includeProperties = null, bool tracked = false)
+		{
+			IQueryable<T> query = tracked ? this.DbSet : this.DbSet.AsNoTracking();
 
-            result = result.Where(filter);
+			if (filter != null)
+			{
+				query = query.Where(filter);
+			}
 
-            if (!string.IsNullOrEmpty(includeProperties))
-            {
-                foreach (var includeProp in includeProperties
-                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
-                {
-                    result = result.Include(includeProp);
-                }
-            }
+			if (!string.IsNullOrEmpty(includeProperties))
+			{
+				foreach (var includeProp in includeProperties
+					.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+				{
+					query = query.Include(includeProp);
+				}
+			}
 
-            return await result.FirstOrDefaultAsync();
-        }
+			var result = await query.FirstOrDefaultAsync();
 
-        public async Task<bool> Remove(T entity)
-        {
-            
-            this.DbSet.Remove(entity);
-            await this._db.SaveChangesAsync();
+			return result;
+		}
 
-            return true;
-        }
 
-        public async Task<bool> RemoveRange(IEnumerable<T> entities)
-        {
-            this.DbSet.RemoveRange(entities);
-            await this._db.SaveChangesAsync();
+		public async Task<bool> Remove(T entity)
+		{
 
-            return true;
-        }
-    }
+			this.DbSet.Remove(entity);
+			await this._db.SaveChangesAsync();
+
+			return true;
+		}
+
+		public async Task<bool> RemoveRange(IEnumerable<T> entities)
+		{
+			this.DbSet.RemoveRange(entities);
+			await this._db.SaveChangesAsync();
+
+			return true;
+		}
+	}
 }
