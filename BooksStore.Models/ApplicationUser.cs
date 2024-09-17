57cb78f9ff26fed2c0543b389587f0af604cea7a
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-09-17 22:07:21 +0700

    Add role property only for display into UI

--- BooksStore.Models/ApplicationUser.cs
+++ BooksStore.Models/ApplicationUser.cs
@@ -34,5 +34,8 @@ namespace BooksStore.Models
         [ForeignKey("CompanyId")]
         [ValidateNever]
         public Company? Company { get; set; }
+
+        [NotMapped]
+        public string? Role { get; set; }
     }
 }
