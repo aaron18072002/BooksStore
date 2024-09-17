c1964e3e82698feed008e779923ef130f9a98dcd
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-09-17 21:29:08 +0700

    Create endpoint for display users and api for get all users

---
+++ BooksStore.Web/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using BooksStore.DataAccess.Database;
+using BooksStore.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksStore.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    //[Authorize(Roles = StaticDetails.Role_Admin)]
+    //[Authorize]
+    [Route("[area]/[controller]")]
+    public class UserController : Controller
+    {
+        private readonly BooksStoreDbContext _db;
+        private readonly ILogger<UserController> _logger;
+        public UserController(BooksStoreDbContext db, ILogger<UserController> logger)
+        {
+            this._db = db;
+            this._logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        #region API calls
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = await this._db.ApplicationUsers
+                .Include(u => u.Company).ToListAsync();
+
+            return this.Json(new
+            {
+                Data = users
+            });
+        }
+        #endregion
+    }
+}
